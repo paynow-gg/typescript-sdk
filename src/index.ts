2c691a7437fc18fe5c6849b87d73c45376277005
Beaudene <officialbeaudene@gmail.com>
2025-10-10 22:04:58 +1300

    feat: export schema types for management & storefront
    issue #2

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,10 @@ import { isPayNowError } from "./errors";
 import { createManagementClient } from "./management";
 import { createStorefrontClient } from "./storefront";
 
+import type { components as ManagementComponents } from "./generated/management";
+import type { components as StorefrontComponents } from "./generated/storefront";
+
+export type ManagementSchemas = ManagementComponents["schemas"];
+export type StorefrontSchemas = StorefrontComponents["schemas"];
+
 export { createManagementClient, createStorefrontClient, isPayNowError };
