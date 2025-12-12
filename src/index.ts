c6e031c7e9f742efc6b8365bf22850a96ad8442d
Tristan Chin <tristan.chin@chintristan.io>
2025-12-11 20:58:34 -0500

    Add WebhooPayload type to aggregate all possible WebhookPayloadSchemas under one type union

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,22 @@ import { createStorefrontClient } from "./storefront";
 
 import type { components as ManagementComponents } from "./generated/management";
 import type { components as StorefrontComponents } from "./generated/storefront";
-import type { webhooks as Webhooks } from "./generated/webhooks";
+import type { webhooks as Webhooks, components as WebhooksComponents } from "./generated/webhooks";
 
 export type ManagementSchemas = ManagementComponents["schemas"];
 export type StorefrontSchemas = StorefrontComponents["schemas"];
 export type WebhookPayloadSchemas = ExtractJsonRequestBodies<Webhooks>;
+
+export type WebhookPayload = {
+  [EventType in WebhooksComponents["schemas"]["WebhookEventType"]]: EventType extends keyof Required<WebhookPayloadSchemas>
+    ? Omit<NonNullable<WebhookPayloadSchemas[EventType]>, "event_type"> & {
+        event_type: EventType;
+      }
+    : {
+        event_type: EventType;
+        event_id: string;
+        body: Record<string, unknown>;
+      };
+}[WebhooksComponents["schemas"]["WebhookEventType"]];
 
 export { createManagementClient, createStorefrontClient, isPayNowError };
